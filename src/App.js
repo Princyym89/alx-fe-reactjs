734422af4249311d9fe1e215efd99774934cbe7c
Princyym89 <iamprincessmatsobe@gmail.com>
2025-09-12 00:59:03 -0700

    Update App.js to include all styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+    <div>
+      <Header />
+      <UserProfile
+        name="Princess Matsobe"
+        age="29"
+        bio="Aspiring developer, lifelong learner, and Virtual Assistant."
+      />
+      <MainContent />
+      <Footer />
+
+      <div className="App" style={{ marginTop: '30px' }}>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <RecipeList />
+      </div>
     </div>
   );
 }
