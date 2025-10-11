db8b39aa8b9e7d146c275bb9afc3dc16f3c2d0f2
Princyym89 <iamprincessmatsobe@gmail.com>
2025-10-11 06:41:59 -0700

    Fix dynamic routing pattern for blog posts

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,19 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { AuthProvider } from './components/AuthContext';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import BlogPost from './components/BlogPost';
-import ProtectedRoute from './components/ProtectedRoute';
-import Login from './components/Login';
-
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        {/* ... rest of the code remains the same ... */}
-      </Router>
-    </AuthProvider>
-  );
-}
-
-export default App;
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/blog/:id" element={<BlogPost />} />
+  <Route
+    path="/profile/*"
+    element={
+      <ProtectedRoute>
+        <Profile />
+      </ProtectedRoute>
+    }
+  />
+</Routes>
